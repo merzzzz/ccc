ae3e90f2fc15c56561cd8664ff70b02b61951375
Mergin <formealone5@gmail.com>
2018-09-07 01:50:27 +1000

    fresh commit

---
+++ test/com/javaproject/test/AddressBookAppTestTest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javaproject.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mergin
+ */
+public class AddressBookAppTestTest {
+    
+    public AddressBookAppTestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of initInputs method, of class AddressBookAppTest.
+     */
+    @Test
+    public void testInitInputs() {
+        System.out.println("initInputs");
+        AddressBookAppTest instance = new AddressBookAppTest();
+        instance.initInputs();
+        // TODO review the generated test code and remove the default call to fail.
+   //     fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testJUnitGetAndValidateContactNumber method, of class AddressBookAppTest.
+     */
+    @Test
+    public void testTestJUnitGetAndValidateContactNumber() {
+        System.out.println("testJUnitGetAndValidateContactNumber");
+        AddressBookAppTest instance = new AddressBookAppTest();
+        instance.testJUnitGetAndValidateContactNumber();
+        // TODO review the generated test code and remove the default call to fail.
+     //   fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testJUnitGetAndValidateContactName method, of class AddressBookAppTest.
+     */
+    @Test
+    public void testTestJUnitGetAndValidateContactName() {
+        System.out.println("testJUnitGetAndValidateContactName");
+        AddressBookAppTest instance = new AddressBookAppTest();
+        instance.testJUnitGetAndValidateContactName();
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testJUnitGetAndValidateAddressBookName method, of class AddressBookAppTest.
+     */
+    @Test
+    public void testTestJUnitGetAndValidateAddressBookName() {
+        System.out.println("testJUnitGetAndValidateAddressBookName");
+        AddressBookAppTest instance = new AddressBookAppTest();
+        instance.testJUnitGetAndValidateAddressBookName();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAddContactsInAddressBook method, of class AddressBookAppTest.
+     */
+    @Test
+    public void testTestAddContactsInAddressBook() {
+        System.out.println("testAddContactsInAddressBook");
+        AddressBookAppTest instance = new AddressBookAppTest();
+        instance.testAddContactsInAddressBook();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+}
